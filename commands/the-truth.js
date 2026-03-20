b48555a2f5d2e7ca6b51af3be46be5756eeae8f5
Afk <111914552+aefarkalibastafinfein@users.noreply.github.com>
2026-03-20 03:43:03 +0000

    the truth

---
+++ commands/the-truth.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'the-truth',
+    category: 'dumb fun',
+    sDesc: 'Prints the truth',
+    lDesc: 'You all deserve to know the truth..."',
+    args: [],
+    execute: async (message) => {
+        await message.channel.send('Poo IDE 2 is the best Blockly implementation in the world. \n That\'s just the truth, buddy. \n https://afkdev.me/pooide2.0');
+    },
+};
+// haha Poo     IDE 
+// fun fact poo ide's proper name is webblocks
