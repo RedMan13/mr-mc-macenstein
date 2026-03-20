7c726b88adb09274d5d56a63b4efe26164d07a93
ElectricFuzzball <saavedrajr96@gmail.com>
2026-03-20 16:22:14 -0400

    Update ping-fuzz.js
    ok so now it should turn all pings into regular text

--- commands/ping-fuzz.js
+++ commands/ping-fuzz.js
@@ -2,40 +2,53 @@ let lastPing = 0;
 const { AttachmentBuilder } = require('discord.js');
 
 const toWait = 2 * 60 * 1000;
+
 module.exports = {
     name: 'electricfuzzping',
     category: 'dumb fun',
     sDesc: 'Pings electricfuzzball',
     lDesc: 'Pings <@1203782668928421949> 6 or 7 times randomly',
-    // hoping this works lel
     args: [
         {
             type: 'string',
-            //lBraket: '{',
-            //rBraket: '}',
             name: 'text',
             desc: 'Optional message to send after each ping',
             required: false
         }
     ],
+
     /**
      * @param {import('discord.js').Message} message
      */
     execute: async (message) => {
-        if ((Date.now() - lastPing) < toWait) return message.reply(`Cooldown ends <t:${Math.round((Date.now() + (toWait - (Date.now() - lastPing))) / 1000)}:R>`);
+        if ((Date.now() - lastPing) < toWait) {
+            return message.reply(
+                `Cooldown ends <t:${Math.round((Date.now() + (toWait - (Date.now() - lastPing))) / 1000)}:R>`
+            );
+        }
+
         lastPing = Date.now();
 
         const args = message.arguments;
-        const extraMessage = args['text'] ? ' ' + args['text'] : '';
+        const rawText = args['text'] || '';
+
+        // detect pingies
+        const containsPing =
+            /<@!?&?\d+>|@everyone|@here/.test(rawText);
+
+        const extraMessage = rawText ? ' ' + rawText : '';
 
         const pingCount = Math.random() < 0.5 ? 6 : 7;
 
-        // pings
         for (let i = 0; i < pingCount; i++) {
-            await message.channel.send(`<@1203782668928421949>${extraMessage}`);
+            await message.channel.send({
+                content: `<@1203782668928421949>${extraMessage}`,
+                allowedMentions: containsPing
+                    ? { users: ['1203782668928421949'] } // ping whitelist
+                    : undefined
+            });
         }
 
-        // ping count thing
         const imageUrl = pingCount === 6
             ? 'http://floppydisk-osc.github.io/random-assets/sixSeven.png'
             : 'http://floppydisk-osc.github.io/random-assets/67_2.jpg';
@@ -48,4 +61,3 @@ module.exports = {
         });
     },
 };
-//hi ddededodediamante if you see this
