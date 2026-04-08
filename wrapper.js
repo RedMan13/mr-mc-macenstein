6dcaa5e37b50630e96dfe5951695eb5370380efe
RedMan13 <godslayerakp@gmail.com>
2026-04-08 11:01:13 -0700

    abcded

--- wrapper.js
+++ wrapper.js
@@ -6,16 +6,16 @@ const spawn = () => {
     bot.on('message', msg => {
         if (msg.stop) allowStop = true;
         if (msg.pull) child.exec('git pull', (err, stdout, stderr) => {
-            if (stdout.includes('Already up to date.')) return process.send({ noChanges: true });
+            if (stdout.includes('Already up to date.')) return bot.send({ noChanges: true });
             const lines = stdout.split(/\r?\n\r?/g);
             const errors = lines.filter(line => line.startsWith('error:'));
-            if (err || stderr || stdout.includes('error:')) return process.send({ couldntMerge: err || stderr || errors });
+            if (err || stderr || stdout.includes('error:')) return bot.send({ couldntMerge: err || stderr || errors });
             const idx = lines.findIndex(line => /files? changed/.test(line));
             const length = parseInt(lines[idx].split(' ')[1]);
             const files = lines.slice(idx - length, idx)
                 .map(line => line.split(' | ')[0].trim());
-            if (files.some(file => !file.startsWith('commands'))) return process.send({ restartNeeded: true, updated: files });
-            process.send({ updated: files });
+            if (files.some(file => !file.startsWith('commands'))) return bot.send({ restartNeeded: true, updated: files });
+            bot.send({ updated: files });
         });
     })
     bot.on('exit', () => {
