1b86239f19981dc98c985a0868c0e12f9b0d0c3d
RedMan13 <godslayerakp@gmail.com>
2026-07-03 23:01:42 -0700

    sigma

--- statics/self-rating.js
+++ statics/self-rating.js
@@ -36,5 +36,5 @@ module.exports = function rate(initiator) {
     if (ping > 20000) ratings.splice(0, ratings.length, 0);
 
     const available = ratings.reduce((c,v) => c + v, 0) * (1 / ratings.length);
-    return { available, ping, commands: Object.entries(dbs.commands).filter(([n, command]) => command.work >= available).map(v => v[0]) };
+    return { available, ping, commands: Object.entries(dbs.commands).filter(([n, command]) => command.work <= available).map(v => v[0]) };
 }
