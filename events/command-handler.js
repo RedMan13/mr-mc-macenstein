de6a8a12f3eda770b1d2c3a08b6197cebd792b0d
RedMan13 <godslayerakp@gmail.com>
2026-07-19 21:19:21 -0700

    tada

--- events/command-handler.js
+++ events/command-handler.js
@@ -7,7 +7,8 @@ module.exports = {
         if (message.content.startsWith(dbs.commandConfig.prefix)) {
             const args = message.content.split(' ');
             const command = args.shift().slice(dbs.config.commands.prefix.length);
-            if (!dbs.commands[command]?.enabled) return;
+            if (!dbs.commands[command]) return message.react('<:no:1164832595478069299>');
+            if (!dbs.commands[command].enabled) return;
             const commandData = dbs.commands[command].command;
             message.args = args.join(' ');
             message.arguments = dbs.commands[command].useCLI
