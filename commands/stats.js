c2bc75c1b2c6354e62d2c382cfc02e763518364e
RedMan13 <godslayerakp@gmail.com>
2026-08-10 16:49:29 -0700

    make stats ecplain itself better

--- commands/stats.js
+++ commands/stats.js
@@ -78,7 +78,10 @@ module.exports = {
                         { name: 'CPU Usages', value: String(rating.usages.map(v => `${v.user.toFixed(0)}%`).join(', ')) },
                         { name: 'Free memory', value: makeMega(rating.freeMem) },
                         { name: 'Rating', value: `${['N/A', 'Terrible', 'Meh', 'Perfect'][Math.floor(rating.available)]} (${rating.available}) (${rating.ratings.map(v => `${v[0]}: ${v[1]}`).join(', ')})` },
-                        { name: 'Commands', value: rating.commands.join(',') }
+                        { name: 'Commands', value: rating.commands
+                            .sort((a,b) => dbs.commands[a].enabled - dbs.commands[b].enabled)
+                            .map(v => dbs.commands[v].enabled ? `**${v}**` : `--${v}--`)
+                            .join(',') }
                     ])
                     .setFooter({ text: 'Bot id: ' + dbs.id + '; Alias: ' + dbs.alias })
             ]
