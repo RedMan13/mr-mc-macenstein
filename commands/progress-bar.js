20ba96a5040864bd2c549d6fc422ffda5b25ab21
RedMan13 <godslayerakp@gmail.com>
2026-09-12 17:12:13 -0700

    change words

--- commands/progress-bar.js
+++ commands/progress-bar.js
@@ -19,7 +19,7 @@ module.exports = {
             max: Infinity,
             min: 0,
             name: 'time',
-            desc: 'How long should the progress bar take to complete',
+            desc: 'What the maximum time to wait between movements of the bar should be',
             required: false
         },
         {
